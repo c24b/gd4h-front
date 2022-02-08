059678cbbd53c7e450693c341a58deeb60ce3645
elisabethfainstein <elisabeth.fainstein@data.gouv.fr>
2022-02-08 08:57:27 +0100

    (styles) add styling to dataset card

--- components/datasets/DatasetCard.js
+++ components/datasets/DatasetCard.js
@@ -1,7 +1,9 @@
 import { Col, Container, Row, Text } from '@dataesr/react-dsfr';
+import ArrowRightLineIcon from 'remixicon-react/ArrowRightLineIcon';
 import { displayWordsWithPipes } from '../../lib/utils';
 
 import styles from './DatasetCard.module.css';
+import utilStyles from '../../styles/utils.module.css';
 
 const DatasetCard = ({ dataset }) => {
 
@@ -18,28 +20,31 @@ const DatasetCard = ({ dataset }) => {
     }
 
     return (
-        <Container>
+        <Container className={`${utilStyles.boxShadow} ${utilStyles.noHorizontalPadding}`} spacing="my-8w">
             <Row className={styles.cardHead} spacing="px-4w py-2w">
-                <Col>
+                <Col n={"10"}>
                     <Row>
                         <h4>{dataset.name}</h4>
                     </Row>
                     <Row>
                         <Col>{displayAllOrganizationsNames(dataset.organizations)}</Col>
-                        <Col>{displayWordsWithPipes([dataset.is_opendata ? "Données ouvertes" : "Données non ouvertes",
+                        <Col n={"10"}>{displayWordsWithPipes([dataset.is_opendata ? "Données ouvertes" : "Données non ouvertes",
                         dataset.downloadable ? "Téléchargeable en ligne" : "Non téléchargeable en ligne"])}
 
                         </Col>
                     </Row>
                 </Col>
-                <Col>-></Col>
+                <Col className={utilStyles.textAlignRight}>
+                    {/* Icons in dataesr/dsfr are pretty broken for now, so we use remixicon directly instead */}
+                    <ArrowRightLineIcon size={38} className={utilStyles.blueFrance} />
+                </Col>
             </Row>
-            <Row>
-                <Col>
-                    <Row>Milieux&nbsp;:&nbsp;{displayWordsWithPipes(dataset.environment)}</Row>
+            <Row spacing={"p-4w"}>
+                <Col n={"5"} spacing={"mr-6w"}>
+                    <Row spacing={"mb-4w"}><span className={utilStyles.bold}>Milieux</span>&nbsp;:&nbsp;{displayWordsWithPipes(dataset.environment)}</Row>
                     <Row className={styles.description}>{dataset.description}</Row>
                 </Col>
-                <Col className={styles.data}>
+                <Col n={"4"} className={styles.data} >
                     <DatasetCardElement label={"Couverture spatiale"} data={"bibibi"} />
                     <DatasetCardElement label={"Granularité de la couverture spatiale"} data={"blabla"} />
                     <DatasetCardElement label={"Données géospatialisées"} data={"blabla"} />
@@ -56,10 +61,10 @@ const DatasetCard = ({ dataset }) => {
 
 const DatasetCardElement = ({ label, data }) => {
     return (
-        <>
-            <Text>{label}</Text>
+        <div className={styles.dataElement}>
+            <Text className={utilStyles.bold} spacing={"m-0"}>{label}</Text>
             <Text spacing={"m-0"}>{data}</Text>
-        </>
+        </div>
     );
 };
 
